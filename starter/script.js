03fad522114c36ba8cda9b574c3b4038abab6069
PRATHAM KUMAR <prathamku3041@gmail.com>
2025-12-07 15:30:13 +0530

    complete

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,19 @@
 document.getElementById("info-btn").addEventListener("click", () => {
     alert("JS effect working!");
 });
+
+const nameElement = document.getElementById('name');
+
+document.addEventListener('mousemove', (e) => {
+    const { clientX, clientY } = e;
+    const { innerWidth, innerHeight } = window;
+
+    const xMove = ((clientX / innerWidth) - 0.5) * 20;
+    const yMove = ((clientY / innerHeight) - 0.5) * 20;
+
+    nameElement.style.transform = `perspective(500px) rotateX(${yMove * -1}deg) rotateY(${xMove}deg)`;
+});
+
+document.addEventListener('mouseleave', () => {
+    nameElement.style.transform = 'perspective(500px) rotateX(0deg) rotateY(0deg)';
+});
